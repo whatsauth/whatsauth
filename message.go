c6abdc2d3317c4f8d20d63b3e7e5dab59b5ea1be
Jose Chasey Pratama <josepratama080@gmail.com>
2023-02-04 08:49:18 +0700

    create default button if button slice is empty, create unique button

--- message.go
+++ message.go
@@ -26,11 +26,24 @@ func GenerateButtonMessage(header string, content string, footer string) (btnmsg
 }
 
 func GenerateButtonMessageCustom(header string, content string, footer string, button []string) (btnmsg atmodel.ButtonsMessage) {
+	if len(button) == 0 {
+		btnmsg = GenerateButtonMessage(header, content, footer)
+		return
+	}
+	keys := make(map[string]bool, len(button))
+	uniq := make([]string, 0, len(button))
+	for _, entry := range button {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			uniq = append(uniq, entry)
+		}
+	}
+
 	btnmsg.Message.HeaderText = header
 	btnmsg.Message.ContentText = content
 	btnmsg.Message.FooterText = footer
-	butt := make([]atmodel.WaButton, 0, len(button))
-	for _, v := range button {
+	butt := make([]atmodel.WaButton, 0, len(uniq))
+	for _, v := range uniq {
 		butt = append(butt, atmodel.WaButton{
 			ButtonId:    v,
 			DisplayText: strings.ToTitle(v),
