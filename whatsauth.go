6e08df5bf20939a3d2378cb82471268108c999b0
Rolly Maulana  Awangga <rolly@awang.ga>
2023-03-15 16:48:35 +0700

    update

--- whatsauth.go
+++ whatsauth.go
@@ -62,10 +62,13 @@ func ButtonMessageWhatsauth(waclient *whatsmeow.Client, Info *types.MessageInfo,
 	ntfbtn := atapi.PostStruct[atmessage.NotifButton](wareq, urlwauthrole)
 	fmt.Println(ntfbtn)
 	btm := ntfbtn.Message
-	resp, err := atmessage.SendButtonMessage(btm, Info.Sender, waclient)
-	fmt.Println(resp)
-	fmt.Println(err)
-
+	if btm.Message.HeaderText == "" {
+		resp, err := atmessage.SendButtonMessage(btm, Info.Sender, waclient)
+		fmt.Println(resp)
+		fmt.Println(err)
+	} else {
+		fmt.Println("struct kembalian kosong")
+	}
 }
 
 func FilterWhatsauthButton(Info *types.MessageInfo, Message *waProto.Message) (filter bool) {
