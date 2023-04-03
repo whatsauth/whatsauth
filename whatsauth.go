00034573a90a2d782b69ccdee255ee4a00fdc413
Rolly Maulana  Awangga <rolly@awang.ga>
2023-04-03 14:17:41 +0700

    add reply wa message regular

--- whatsauth.go
+++ whatsauth.go
@@ -26,6 +26,19 @@ func HasKeyword(Info *types.MessageInfo, Message *waProto.Message) (whmsg bool) 
 	return
 }
 
+func HasRoleKeyword(Info *types.MessageInfo, Message *waProto.Message) (whmsg bool) {
+	if Message.ExtendedTextMessage != nil && Info.Chat.Server == "s.whatsapp.net" {
+		if strings.Contains(*Message.ExtendedTextMessage.Text, Keyword+".") {
+			if Message.ExtendedTextMessage.ContextInfo != nil {
+				if *Message.ExtendedTextMessage.ContextInfo.EntryPointConversionSource == "click_to_chat_link" {
+					whmsg = true
+				}
+			}
+		}
+	}
+	return
+}
+
 func RunModule(waclient *whatsmeow.Client, Info *types.MessageInfo, Message *waProto.Message, urlwauthreq string, prefixurlapiwa string) {
 	var wareq WhatsauthRequest
 	wareq.Uuid = strings.Replace(*Message.ExtendedTextMessage.Text, Keyword, "", 1)
@@ -44,8 +57,34 @@ func RunModule(waclient *whatsmeow.Client, Info *types.MessageInfo, Message *waP
 func HandlerWhatsauth(waclient *whatsmeow.Client, Info *types.MessageInfo, Message *waProto.Message, urlwauthreq string, urlwauthrole string, prefixurlapiwa string) {
 	if Message.ButtonsResponseMessage != nil {
 		ButtonMessageWhatsauth(waclient, Info, Message, urlwauthrole)
+	} else if HasRoleKeyword(Info, Message) {
+		MessageWhatsauth(waclient, Info, Message, urlwauthrole)
 	} else {
 		RunModule(waclient, Info, Message, urlwauthreq, prefixurlapiwa)
+	}
+}
+
+func MessageWhatsauth(waclient *whatsmeow.Client, Info *types.MessageInfo, Message *waProto.Message, urlwauthrole string) {
+	wareq := new(WhatsAuthRoles)
+	uuidSplit := strings.SplitN(*Message.ExtendedTextMessage.Text, ".", 3)
+	if len(uuidSplit) < 2 {
+		atmessage.SendMessage("Error, len split UUID from ButtonID not enough.", Info.Chat, waclient)
+
+	}
+	wareq.Uuid = uuidSplit[2]
+	wareq.Roles = uuidSplit[1]
+	wareq.Phonenumber = Info.Sender.User
+	fmt.Println(wareq)
+	ntfbtn := atapi.PostStruct[atmessage.NotifButton](wareq, urlwauthrole)
+	fmt.Println(ntfbtn)
+	btm := ntfbtn.Message
+	if btm.Message.HeaderText != "" {
+		atmessage.SendMessage(ButtonMessageToMessage(btm, ""), Info.Sender, waclient)
+		resp, err := atmessage.SendButtonMessage(btm, Info.Sender, waclient)
+		fmt.Println(resp)
+		fmt.Println(err)
+	} else {
+		fmt.Println("struct kembalian kosong")
 	}
 }
 
