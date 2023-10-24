f3ac296456579346c1b74cf786f09f50a0d8dcd2
JPratama7 <josepratama080@gmail.com>
2023-10-24 12:24:53 +0700

    feature(RunSocketGenerator): create socket tanpa cek database

--- run.go
+++ run.go
@@ -196,3 +196,49 @@ func SelectedRoles(
 	return
 
 }
+
+func RunSocketGenerator(c *websocket.Conn) (Id string) { // call this function after declare URL routes
+	var s Client
+	// When the function returns, unregister the client and close the connection
+	defer func() {
+		Unregister <- s.Id
+		c.Close()
+	}()
+	messageType, message, err := c.ReadMessage()
+	if err != nil {
+		if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+			log.Println("read error:", err)
+		}
+		return // Calls the deferred function, i.e. closes the connection on error
+	}
+	Id = string(message)
+	if messageType == websocket.TextMessage {
+		// Get the received message
+		// Register the client
+		s = Client{
+			Id:   Id,
+			Conn: c,
+		}
+		Register <- s
+		for {
+			messageType, message, err := s.Conn.ReadMessage()
+			if err != nil {
+				if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+					log.Println("read error:", err)
+				}
+				return // Calls the deferred function, i.e. closes the connection on error
+			}
+
+			if messageType == websocket.TextMessage {
+				// log the received message
+				log.Println(string(message))
+			} else {
+				log.Println("websocket message received of type", messageType)
+			}
+		}
+	} else {
+		log.Println("websocket message received of type", messageType)
+	}
+	return
+
+}
